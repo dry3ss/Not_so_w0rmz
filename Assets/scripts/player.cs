09b35236626985350d8b3f49768144d8e774edcf
dry3ss <willvi31@yahoo.fr>
2017-02-28 20:12:56 +0100

    with ennemy

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Vector2 speed = new Vector2(20, 20);
+
+    private Vector2 movement;
+    private Rigidbody2D rigidbodyComponent;
+
+
+    // Use this for initialization
+    void Start () {	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 3 - Retrieve axis information
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        // 4 - Movement per direction
+        movement = new Vector2(
+          speed.x * inputX,
+          speed.y * inputY);
+
+    }
+    void FixedUpdate()
+    {
+        // 5 - Get the component and store the reference
+        if (rigidbodyComponent == null) rigidbodyComponent = GetComponent<Rigidbody2D>();
+
+        // 6 - Move the game object
+        rigidbodyComponent.velocity = movement;
+    }
 }

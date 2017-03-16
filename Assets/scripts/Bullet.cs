c1f8d6e1c288c9dfff72128a02df4fb2e453f1e2
thomas olivares <thomas olivares>
2017-03-16 18:41:38 +0100

    add bullet damages

--- Assets/scripts/Bullet.cs
+++ Assets/scripts/Bullet.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class Bullet : MonoBehaviour {
 
 
-    public int damage = 1;
+    public float damageInstant = 1;
+    public float damageAreaMax = 5;
+    public float damageAreaMin = 3;
+
+    public int radius = 2;
+
     // Use this for initialization
     void Start()
     {
         // 2 - Limited time to live to avoid any leak
-        Destroy(gameObject, 3); // 20sec
+        Destroy(gameObject, 20); // 20sec
     }
 
     // Update is called once per frame
@@ -24,12 +29,37 @@ public class Bullet : MonoBehaviour {
         Health player = otherCollider.gameObject.GetComponent<Health>();
         if (player != null)
         {
-            player.Damage(damage);
+            player.Damage((int)damageInstant);
         }
+        else
+        {
+            // It's the floor, we check the area damages
+            Collider2D[] allUnit = Physics2D.OverlapCircleAll(new Vector2(this.transform.position.x, this.transform.position.y), radius);
 
-        Collider[] allUnit = Physics.OverlapSphere(this.transform.position, 1);
-
+            for (int i = 0; i < allUnit.Length; i++)
+            {
+                applyDamages(allUnit[i], this.transform.position);
+            }
+        }
+        
         // Destroy the shot
         Destroy(this.gameObject); // Remember to always target the game object, otherwise you will just remove the script
+    }
+
+    void applyDamages(Collider2D entity, Vector2 positionBullet)
+    {
+        Vector2 positionCible = new Vector2(entity.transform.position.x, entity.transform.position.y);
+        float distance = Vector2.Distance(positionCible, positionBullet);
+
+        // Bug to fix : the distance is beetween the 2 origin, which are not the centers but the corners
+        if (distance < radius)
+        {
+            Health player = entity.gameObject.GetComponent<Health>();
+            int damages = (int)((damageAreaMax - damageAreaMin) * (radius - distance) / radius);
+            if (player != null)
+            {
+                player.Damage(damages);
+            }
+        }
     }
 }
